bc1a91fd29d9d5c3aaad05a796cbad07109a9d8b
vitorhugo425 <149179118+vitorhugo425@users.noreply.github.com>
2023-11-13 09:49:26 -0300

    Meu Primeiro Programa.js

---
+++ Meu Primeiro Programa.js
@@ -0,0 +1,17 @@
+function setup() {
+  createCanvas(600, 600);
+  background("green")
+}
+
+function draw() {
+  
+
+  stroke("red");
+  fill("blue");
+  
+  //console.log (mouseIsPressed);
+  
+  if (mouseIsPressed) {
+    rect(mouseX, mouseY, 20, 35);
+  }
+}
